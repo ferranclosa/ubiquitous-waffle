c47c10c37dbc4c6d13c6ae8a20ed12d919dc1f5e
ferranclosa <ferranclosa@gmail.com>
2020-12-01 10:27:35 +0100

    Added OAuth2 recognizer functionality

--- src/main/java/com/closa/data/DataApplication.java
+++ src/main/java/com/closa/data/DataApplication.java
@@ -1,11 +1,18 @@
 package com.closa.data;
 
+import com.closa.global.context.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.closa.global.*",  "com.closa.data.*", "com.closa.global.trace.*"})
@@ -16,6 +23,23 @@ public class DataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataApplication.class, args);
+    }
+
+    @SuppressWarnings("unchecked")
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors==null){
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        }
+        else{
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
     }
 
 }
