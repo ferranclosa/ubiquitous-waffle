76990756707938496fa91170f4add66afa294244
ferraclosa <ferraclosa@gmail.com>
2021-06-25 11:02:12 +0200

    Push to sync with MB

--- src/main/java/com/closa/data/DataApplication.java
+++ src/main/java/com/closa/data/DataApplication.java
@@ -3,12 +3,14 @@ package com.closa.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 @ComponentScan(basePackages = {"com.closa.global.*",  "com.closa.data.*", "com.closa.global.trace.*"})
 @EntityScan(basePackages = {"com.closa.global.status.model", "com.closa.data.model", "com.closa.global.trace.model"})
 @EnableJpaRepositories(basePackages = {"com.closa.global.status.dao", "com.closa.data.dao", "com.closa.global.trace.dao"})
